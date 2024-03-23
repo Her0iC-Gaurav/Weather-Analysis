e007a84e1ae245f7029436beb6943b60a0188541
Her0iC-Gaurav <pgywww@gmail.com>
2024-03-24 03:37:20 +0530

    Done till 7th Power BI Questions

--- Solution/SQL Script/Question 15.sql
+++ Solution/SQL Script/Question 15.sql
@@ -6,17 +6,14 @@ SELECT
         WHEN f.weather_description IN ('hurricane', 'tornado', 'ragged thunderstorm', 
         'thunderstorm with heavy rain', 'thunderstorm with heavy drizzle', 
         'thunderstorm with heavy drizzle', 'thunderstorm with heavy rain', 
-        'heavy thunderstorm', 'thunderstorm with rain', 'thunderstorm with light rain', 
-        'thunderstorm with light drizzle') 
+        'heavy thunderstorm', 'thunderstorm with rain', 'thunderstorm with light rain', 'thunderstorm with light drizzle') 
         THEN 'Severe'
         WHEN f.weather_description IN ('storm', 'heavy intensity rain', 'heavy snow', 
-        'heavy shower snow', 'heavy intensity shower rain', 'heavy intensity drizzle', 
-        'heavy intensity drizzle rain', 'sleet', 'freezing rain', 'squalls') 
+        'heavy shower snow', 'heavy intensity shower rain', 'heavy intensity drizzle', 'heavy intensity drizzle rain', 'sleet', 'freezing rain', 'squalls') 
         THEN 'Moderate'
         WHEN f.weather_description IN ('mist', 'fog', 'dust', 'smoke', 'haze', 'sand', 
         'volcanic ash', 'sand/dust whirls', 'proximity sand/dust whirls', 'ragged shower rain', 
-        'proximity shower rain', 'proximity moderate rain', 'proximity thunderstorm', 
-        'proximity thunderstorm with drizzle', 'proximity thunderstorm with rain') 
+        'proximity shower rain', 'proximity moderate rain', 'proximity thunderstorm', 'proximity thunderstorm with drizzle', 'proximity thunderstorm with rain') 
         THEN 'Low'
         ELSE 'Normal'
     END AS event_severity,
@@ -31,14 +28,19 @@ WHERE
     ((c.Country = 'USA' AND ca.Latitude BETWEEN 24 AND 49 AND ca.Latitude IS NOT NULL) -- Latitude range for USA
     OR (c.Country = 'Canada' AND ca.Latitude BETWEEN 42 AND 83 AND ca.Latitude IS NOT NULL) -- Latitude range for Canada
     OR (c.Country = 'Israel' AND ca.Latitude BETWEEN 29 AND 33 AND ca.Latitude IS NOT NULL)) -- Latitude range for Israel
-    AND EXISTS (
+    AND f.wind_direction IS NOT NULL 
+    AND f.weather_description IS NOT NULL 
+    AND f.wind_speed IS NOT NULL
+    AND 
+    EXISTS (
         SELECT 1 
         FROM city_attributes AS ca_coastal
         WHERE 
             ca_coastal.City_id = ca.City_id 
             AND (ca_coastal.Latitude BETWEEN -90 AND 90)  -- Assuming -90 to 90 latitude represents coastal cities
             AND (ca_coastal.Longitude BETWEEN -180 AND 180)  -- Assuming -180 to 180 longitude represents coastal cities
-    )
+    ) 
+    
 GROUP BY 
     f.weather_description,
     f.wind_speed,
